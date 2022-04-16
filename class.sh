6dc675ba1919d0c3983b4c9ec83a53068a88614a
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-04-16 00:29:12 +0000

    done with .cpp files

--- class.sh
+++ class.sh
@@ -7,17 +7,20 @@
 
 for var in $@
 do
+	# Get the class name in Uppercase and Capitalized from filename
 	classU=${var^^}
 	classC=${var^}
 
+	# Write in the header file the definition guards
 	echo -e \
 		"\n#ifndef	${classU}_HPP\n\
 #ifdef	${classU}_HPP\n" >> $classC.hpp
 
+	# Write in the header file the class Definition
 	echo -e \
 		"class	${classC}\n\
 {\n\
-private:\n\
+private:\n\t\n\
 public:\n\
 \t${classC} ( void );\n\
 \t~${classC} ( void );\n\
@@ -25,6 +28,15 @@ public:\n\
 \t${classC} operator=( const ${classC}& ${classC,,} );\n\
 }\n" >> $classC.hpp
 
+	# Write in the .cpp file the function implementation
+	echo -e \
+"\n${classC}::${classC} ( void ) { }\n\
+${classC}::~${classC} ( void ) { }\n\
+${classC}::${classC} ( const ${classC}& ${classC,,} ) { }\n\
+${classC}::${classC} operator=( const ${classC}& ${classC,,} ) { }\n\n\
+}" >> $classC.cpp
+
+	# Add headers both in .hpp and .cpp files
 	nvim -c "AddHeader" -c wq $classC.hpp
 	nvim -c "AddHeader" -c wq $classC.cpp
 done
