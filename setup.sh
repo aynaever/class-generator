a754ffd324cb753d4bbf8357cc7f7f3e41b4481d
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-04-16 07:01:53 +0000

    finish setup.sh that adds Makefile

--- setup.sh
+++ setup.sh
@@ -6,3 +6,29 @@
 # Last Modified By  : Aymane N <anaouadi@students.42wolfsburg.de>
 
 bash -c "./class.sh $@"
+
+for var in $@
+do
+	# Get the class name in Capitalized from filename
+	classC=${var^}
+	FILES="$FILES ${classC}.cpp"
+done
+
+echo -e \
+	"\nNAME = main\n\
+CFILES = main.cpp $FILES\n\
+FLAGS = -Wall -Werror -Wextra -std=c++98\n\n\
+all:\n\
+	c++ \$(CFILES) -o \$(NAME)\n\n\
+run: all\n\
+	./\$(NAME)\n\n\
+clean:\n\
+	rm -rf *.o\n\n\
+fclean: clean\n\
+	rm -rf \$(NAME)\n\n\
+re: fclean all\n\n\
+.PHONY: clean fclean re all\
+" > Makefile
+
+# Add Header to Makefile
+nvim -c "AddHeader" -c wq Makefile
