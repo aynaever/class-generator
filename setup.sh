7fdc3a4ee1456ecfeba337a71c7d35abadfcda8d
Aymane Naouadir <aymane.naouadir18@gmail.com>
2022-04-16 06:40:36 +0000

    add class.sh to setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# File              : setup.sh
+# Author            : Aymane N <anaouadi@students.42wolfsburg.de>
+# Date              : 16.04.2022
+# Last Modified Date: 16.04.2022
+# Last Modified By  : Aymane N <anaouadi@students.42wolfsburg.de>
+
+bash -c "./class.sh $@"
